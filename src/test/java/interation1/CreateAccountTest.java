36c2f4f6febc47d8b4f43c867267e8c8029b4843
Dasha Khmelkova <khmelkovadaria@gmail.com>
2025-09-29 20:51:47 +0300

    add api autotests

---
+++ src/test/java/interation1/CreateAccountTest.java
@@ -0,0 +1,74 @@
+package interation1;
+
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+
+public class CreateAccountTest {
+    @BeforeAll
+    public static void setUpRestAssured() {
+        RestAssured.filters(
+                List.of(new RequestLoggingFilter(),
+                        new ResponseLoggingFilter())
+        );
+    }
+
+    @Test
+    public void userCanCreateAccountTest() {
+        // Create user
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .header("Authorization", "Basic YWRtaW46YWRtaW4=")
+                .body("""
+                        {
+                          "username": "kate20001qQ",
+                          "password": "kate1qQ$",
+                          "role": "USER"
+                        }
+                        """)
+                .post("http://localhost:4111/api/v1/admin/users")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_CREATED)
+                .body("username", Matchers.equalTo("kate20001qQ"))
+                .body("password", Matchers.not(Matchers.equalTo("kate1qQ$")))
+                .body("role", Matchers.equalTo("USER"));
+
+        // Get auth token
+        String userAuthHeader =
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body("""
+                        {
+                          "username": "kate20001qQ",
+                          "password": "kate1qQ$"
+                        }
+                        """)
+                .post("http://localhost:4111/api/v1/auth/login")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .extract()
+                .header("Authorization");
+        // Create account
+        given()
+                .header("Authorization", userAuthHeader)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .post("http://localhost:4111/api/v1/accounts")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_CREATED);
+    }
+}
