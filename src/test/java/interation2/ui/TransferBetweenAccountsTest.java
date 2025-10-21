d5a78207f360a13dfa49b63a17db6b0539350193
Dasha Khmelkova <khmelkovadaria@gmail.com>
2025-10-22 01:32:34 +0300

    fix: review 1

--- src/test/java/interation2/ui/TransferBetweenAccountsTest.java
+++ src/test/java/interation2/ui/TransferBetweenAccountsTest.java
@@ -1,5 +1,6 @@
 package interation2.ui;
 
+import api.generators.RandomModelGenerator;
 import api.models.*;
 import api.requests.steps.*;
 import api.models.comparison.ModelAssertions;
@@ -16,12 +17,12 @@ public class TransferBetweenAccountsTest extends BaseUiTest {
         CreateUserRequest user = AdminSteps.createUser();
         Long idFirst = AccountSteps.createAccountAndGetId(user.getUsername(), user.getPassword());
         Long idSecond = AccountSteps.createAccountAndGetId(user.getUsername(), user.getPassword());
-
-        CreateDepositRequest createDepositRequest = TestData.buildCreateDepositRequest(idFirst,3000);
+        CreateDepositRequest createDepositRequest  = RandomModelGenerator.generate(CreateDepositRequest.class);
+        createDepositRequest.setId(idFirst);
         DepositSteps.createDeposit(user.getUsername(), user.getPassword(), createDepositRequest);
-        CreateTransferRequest createTransferRequest = TestData.buildCreateTransferRequest(idFirst, idSecond, 1000);
-        CreateDepositRequest expectedSender = TestData.buildCreateDepositRequest(idSecond,2000);
-        CreateDepositRequest expectedReceiver = TestData.buildCreateDepositRequest(idFirst,1000);
+        CreateTransferRequest createTransferRequest = TestData.buildCreateTransferRequest(idFirst, idSecond, createDepositRequest.getBalance()/2);
+        CreateDepositRequest expectedSender = TestData.buildCreateDepositRequest(idSecond,(int) (createDepositRequest.getBalance() - createDepositRequest.getBalance()/2));
+        CreateDepositRequest expectedReceiver = TestData.buildCreateDepositRequest(idFirst,(int) (createDepositRequest.getBalance()/2));
 
         String amount =  Double.toString(createTransferRequest.getAmount());
         String idRecipientAccount =  "ACC" + Long.toString(createTransferRequest.getReceiverAccountId());
@@ -71,10 +72,11 @@ public class TransferBetweenAccountsTest extends BaseUiTest {
         Long idFirst = AccountSteps.createAccountAndGetId(user.getUsername(), user.getPassword());
         Long idSecond = AccountSteps.createAccountAndGetId(user.getUsername(), user.getPassword());
 
-        CreateDepositRequest createDepositRequest = TestData.buildCreateDepositRequest(idFirst,3000);
+        CreateDepositRequest createDepositRequest  = RandomModelGenerator.generate(CreateDepositRequest.class);
+        createDepositRequest.setId(idFirst);
         DepositSteps.createDeposit(user.getUsername(), user.getPassword(), createDepositRequest);
-        CreateTransferRequest createTransferRequest = TestData.buildCreateTransferRequest(idFirst, idSecond, 1000);
-        CreateDepositRequest expectedSender = TestData.buildCreateDepositRequest(idSecond,3000);
+        CreateTransferRequest createTransferRequest = TestData.buildCreateTransferRequest(idFirst, idSecond, createDepositRequest.getBalance()/2);
+        CreateDepositRequest expectedSender = TestData.buildCreateDepositRequest(idSecond,(int) (createDepositRequest.getBalance()));
         CreateDepositRequest expectedReceiver = TestData.buildCreateDepositRequest(idFirst,0);
 
         String amount =  Double.toString(createTransferRequest.getAmount());
