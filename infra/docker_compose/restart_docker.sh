e93a87fd09b1fce818dc55c46bf4e95f86c0a063
Dasha Khmelkova <khmelkovadaria@gmail.com>
2025-11-08 01:33:51 +0300

    add: restart docker script

---
+++ infra/docker_compose/restart_docker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo ">>> Остановить Docker Compose"
+docker compose down
+
+echo ">>> Docker pull все образы браузеров"
+
+# путь до файла
+json_file="./config/browsers.json"
+
+# проверяем, что jq установлен
+if ! command -v jq &> /dev/null; then
+    echo "❌ jq is not installed. Please install jq and try again."
+    exit 1
+fi
+
+# извлекаем все значения .image через jq
+images=$(jq -r '.. | objects | select(.image) | .image' "$json_file")
+
+# пробегаем по каждому образу и выполняем docker pull
+for image in $images; do
+    echo "Pulling $image..."
+    docker pull "$image"
+done
+
+echo ">>> Запуск Docker Compose"
+docker compose up
